f1fbb6a68e1b89ebd3ff032c8daa65de398028b3
GreiceTreviso <medicoerro@gmail.com>
2026-04-02 18:07:27 -0300

    Quarto commit
    Commitei errado e as atualizações não foram, agora adicionei elas de novo.

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 
 export default function App() {
   const tarefas =[
     {id: "1", titulo:"Aprender Git"},
-    {id: "2", titulo:"Pegar o Bowser no mc"},
+    {id: "2", titulo:"Roubar balde Yoshi de alguma kid rica"},
     {id: "3", titulo:"Dar todo meu dinheiro pra nintendo"},
     {id: "4", titulo:"Upar a Jane Doe pro lvl 100"},
     {id: "5", titulo:"dormir"}, 
@@ -16,14 +16,16 @@ export default function App() {
 
       <FlatList
         data={tarefas}
-        keyExtractor={(itens)=>itens.id}
+        keyExtractor={(item)=>item.id}
         renderItem={({item})=>(
-          <Text style={{color: 'white'}}>{item.titulo}</Text>
+          <Text style={[styles.item, {color: 'black'}]}>{item.titulo}</Text>
 
-        )}
-        />
+        )}/>
 
+
+    <TouchableOpacity style={styles.btncAdicionar}> <text>Adicionar tarefas</text> </TouchableOpacity>
     </View>
+
   );
 }
 
@@ -38,8 +40,26 @@ const styles = StyleSheet.create({
    fontSize: 35, // tamanho da letra
    fontWeight: 'bold',
    color: "#039c0b",
-   marginBottom: '20', //espaço abaixo
+   marginBottom: 10, //espaço abaixo
    textAlign:'center'
   },
   
+  item:{
+    padding:15,
+    marginTop:15,
+    backgroundColor:'#65a821', //estilo das tarefinhas
+    borderRadius: 15
+  },
+
+  btncAdicionar:{
+    backgroundColor:'#ff0',
+    padding: 20,
+    width: "50%",
+    borderRadius: 5,
+    justifyContent:'center',
+    textAlign:'center',
+    alignSelf:'center', //força alinhar centro
+    marginBottom: 10
+  }
+
 });
